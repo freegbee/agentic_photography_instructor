7b0345b73f934e885a3c382e23d75bb66bbe3326
Holger Heymanns <holger.heymanns@gmail.com>
2025-09-20 12:53:13 +0200

    feature: implement juror to infer score for image

--- test/TestScoring.py
+++ test/TestScoring.py
@@ -2,26 +2,31 @@ import os
 from pathlib import Path
 from typing import Dict
 
+import cv2
 from numpy import ndarray
 
-from utils.ConfigLoader import ConfigLoader
-from utils.Registries import AGENT_FACTORY_REGISTRY
-from utils.TestingUtils import TestingUtils
+from juror import Juror
 import transformation_agent
+from utils.ConfigLoader import ConfigLoader
+from utils.Registries import TRANSFORMER_REGISTRY, AGENT_FACTORY_REGISTRY
+from utils.TestingUtils import TestingUtils
 
 
 def main():
     config: Dict = ConfigLoader().load(env=os.environ["ENV_NAME"])
     image_path = Path.cwd() / Path(config['dev']['single_image'])
+    # Load image in BGR pixel order
     image: ndarray = TestingUtils.load_image_from_path(image_path)
+    juror = Juror()
+    score = juror.inference(image_rgb=cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    print(f"Score for image {image_path}: {score}")
 
     for factory_name in AGENT_FACTORY_REGISTRY.keys():
         for agent in AGENT_FACTORY_REGISTRY.get(factory_name).create_agents():
             image_clone = image.copy()
             transformed_image, label = agent.transform(image_clone)
-            TestingUtils.save_image_to_path(transformed_image, Path.cwd() / Path(
-                config['dev']['temp_outout_dir']) / f"{label}_{image_path.name}")
-
+            score = juror.inference(image_rgb=cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB))
+            print(f"Score for image with transformation '{label}': {score}")
 
 if __name__ == '__main__':
     main()
