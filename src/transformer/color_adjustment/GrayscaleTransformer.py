de6611b8c637b388ef95a928be0ecb563d5dc808
Holger Heymanns <holger.heymanns@gmail.com>
2025-09-20 15:18:33 +0200

    feature: make sure, grayscale will noch change ndarray dimensions

--- src/transformer/color_adjustment/GrayscaleTransformer.py
+++ src/transformer/color_adjustment/GrayscaleTransformer.py
@@ -10,4 +10,5 @@ class GrayscaleTransformer(AbstractColorAdjustmentTransformer):
     description = "Transforms Image to grayscales."
 
     def transform(self, image: ndarray) -> ndarray:
-        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Keep 3 channels but make it grayscale. No transformer may change the number of channels or the color channel order.
+        return cv2.cvtColor(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
