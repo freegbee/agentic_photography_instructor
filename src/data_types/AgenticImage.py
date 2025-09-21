1f483d7b9e7bdbb72841f08f605c24d511f038c4
Holger Heymanns <holger.heymanns@gmail.com>
2025-09-21 13:12:35 +0200

    feature: clearer structure of class AgenticImage.py
    also: nicer output for the test

--- src/data_types/AgenticImage.py
+++ src/data_types/AgenticImage.py
@@ -1,4 +1,4 @@
-from typing import List, Type
+from typing import List
 
 import cv2
 import numpy as np
@@ -6,24 +6,19 @@ from PIL import Image
 from pydantic import BaseModel, field_validator
 
 
-class AgenticImage(BaseModel):
-    filename: str | None = None
-    is_source: bool | None = None
-    image_data: np.ndarray | None = None
-    image_color_order: str | None = None
-    score: float | None = None
-    transformed_score: float | None = None
+class TransformerApplication(BaseModel):
+    """ Class to keep track of applied transformers and their effect on the original image score. """
     applied_transformers: List[str] | None = None
+    score: float | None = None
+    score_change: float | None = None
+
+class ImageData(BaseModel):
+    image_data: np.ndarray | None = None
+    image_color_order: str | None = None  # 'RGB' or 'BGR'
+    score: float | None = None
 
     class Config:
         arbitrary_types_allowed = True
-
-    def add_image(self, image_data: np.ndarray, image_channel_order: str, is_source: bool, filename: str):
-        """ Add image data and related metadata to the AgenticImage instance. """
-        self.image_data = image_data
-        self.image_color_order = image_channel_order
-        self.is_source = is_source
-        self.filename = filename
 
     @field_validator('image_data')
     def validate_image_data(cls, v):
@@ -31,11 +26,12 @@ class AgenticImage(BaseModel):
             raise TypeError("image_data muss ein numpy.ndarray sein")
         return v
 
-    def set_applied_transformers(self, applied_transformers: str | List[str]):
-        if isinstance(applied_transformers, str):
-            self.applied_transformers = applied_transformers.split('&')
-        else:
-            self.applied_transformers = applied_transformers.copy()
+    def clone(self):
+        return ImageData(
+            image_data=self.image_data.copy() if self.image_data is not None else None,
+            image_color_order=self.image_color_order,
+            score=self.score
+        )
 
     def get_image_data(self, image_channel_order=None) -> np.ndarray:
         """ Return the stored image data as a numpy ndarray. Change color order if specified. """
@@ -63,22 +59,78 @@ class AgenticImage(BaseModel):
 
         return Image.fromarray(self.image_data)
 
+
+class AgenticImage(BaseModel):
+    filename: str | None = None
+    source_image: ImageData | None = None
+    transformed_image: ImageData | None = None
+    applied_transformers: List[str] | None = None
+
+    transformer_protocol: List[TransformerApplication] = []
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def update_source_image(self, image_data: np.ndarray, image_color_order: str, filename: str, score: float | None = None):
+        """ Add image data and related metadata to the AgenticImage instance. """
+        self.source_image = ImageData(image_data=image_data,
+                                      image_color_order=image_color_order)
+        self.filename = filename
+        self.update_source_score(score)
+
+    def update_source_score(self, score: float | None):
+        if self.source_image is None:
+            raise ValueError("Source image must be set before setting the score")
+        self.source_image.score = score
+
+    def update_transformed_image(self, image_data: np.ndarray, image_color_order: str,
+                                 applied_transformers: str | List[str], score: float | None = None):
+        """ Add transformed image data and related metadata to the AgenticImage instance. """
+        self.transformed_image = ImageData(image_data=image_data,
+                                           image_color_order=image_color_order)
+        self.update_applied_transformers(applied_transformers)
+        self.update_transformed_score(score)
+
+    @staticmethod
+    def __standardize_applied_transformer(applied_transformer: str | List[str]) -> List[str]:
+        if isinstance(applied_transformer, str):
+            return applied_transformer.split('&')
+        elif isinstance(applied_transformer, list):
+            return applied_transformer
+        else:
+            raise TypeError("applied_transformer must be a string or a list of strings")
+
+    def update_applied_transformers(self, applied_transformers: str | List[str]):
+        self.applied_transformers = self.__standardize_applied_transformer(applied_transformers)
+
+    def update_transformed_score(self, score: float | None):
+        if self.transformed_image is None:
+            raise ValueError("Transformed image must be set before setting the score")
+        self.transformed_image.score = score
+
+    def append_transformer_protocol(self, applied_transformer: str | List[str], score: float):
+        self.transformer_protocol.append(TransformerApplication(
+            applied_transformers=self.__standardize_applied_transformer(applied_transformer),
+            score=score,
+            score_change=score - self.source_image.score if self.source_image is not None and self.source_image.score is not None else None
+        ))
+
     def clone(self):
         """ Create a deep copy of the AgenticImage instance. """
         return AgenticImage(
             filename=self.filename,
-            is_source=self.is_source,
-            image_data=self.image_data.copy() if self.image_data is not None else None,
-            image_color_order=self.image_color_order,
-            score=self.score,
-            transformed_score=self.transformed_score,
+            source_image=self.source_image.clone() if self.source_image is not None else None,
+            transformed_image=self.transformed_image.clone() if self.transformed_image is not None else None,
             applied_transformers=self.applied_transformers.copy() if self.applied_transformers is not None else None
         )
 
+    def __ensure_scores(self):
+        if self.source_image is None or self.source_image.score is None or self.transformed_image is None or self.transformed_image.score is None:
+            raise ValueError("Both source and transformed scores must be set to compare")
+
     def calculate_score_change(self):
-        if self.score is not None and self.transformed_score is not None:
-            return self.transformed_score - self.score
-        return None
+        self.__ensure_scores()
+        return self.transformed_image.score - self.source_image.score
 
     def has_transformations(self) -> bool:
         """ Check if any transformations have been applied to the image. """
@@ -86,15 +138,14 @@ class AgenticImage(BaseModel):
 
     def transform_is_better(self) -> bool:
         """ Check if the transformed score is better than the original score. """
-        if self.score is None or self.transformed_score is None:
-            raise ValueError("Both original and transformed scores must be set to compare")
-        return self.transformed_score > self.score
+        self.__ensure_scores()
+        return self.transformed_image.score > self.source_image.score
 
     def get_comparable_score(self):
-        return self.transformed_score if self.transformed_score is not None else self.score
+        return self.transformed_image.score if self.transformed_image is not None and self.transformed_image.score is not None else self.source_image.score if self.source_image is not None else None
 
     def is_better_than(self, other_image) -> bool:
         """ Determine if the current image is better than another image based on their scores. """
         if self.get_comparable_score() is None or other_image.get_comparable_score() is None:
-            raise ValueError("Both images must have a transformed score to compare")
+            raise ValueError("Both images must have a  score to compare")
         return self.get_comparable_score() > other_image.get_comparable_score()
--- src/utils/Registries.py
+++ src/utils/Registries.py
@@ -2,3 +2,8 @@ from class_registry import ClassRegistry
 
 TRANSFORMER_REGISTRY = ClassRegistry('label')
 AGENT_FACTORY_REGISTRY = ClassRegistry('factory_name')
+
+def init_registries():
+    import transformer
+    import transformation_agent
+    # The imports above will register all transformers and agent factories
