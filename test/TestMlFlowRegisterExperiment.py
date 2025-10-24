dd7baf336f985d7b32610be16931e6a9e2c5b5d0
Holger Heymanns <holger.heymanns@gmail.com>
2025-10-24 16:53:21 +0200

    feat: work in progress - test mlflow experiment

---
+++ test/TestMlFlowRegisterExperiment.py
@@ -0,0 +1,31 @@
+import os
+from mlflow import MlflowClient
+
+def main():
+    # FIXME adjust port accoring to config
+    print('Trying to start...')
+
+    mlflow_tracking_url=os.environ["MLFLOW_TRACKING_URI"]
+    client = MlflowClient(tracking_uri=mlflow_tracking_url)
+
+    # Provide an Experiment description that will appear in the UI
+    experiment_description = (
+        "Agantic Photography Instructor MLFlow PoC"
+    )
+
+    # Provide searchable tags that define characteristics of the Runs that
+    # will be in this Experiment
+    experiment_tags = {
+        "project_name": "api_poc",
+        "mlflow.note.content": experiment_description,
+    }
+
+    # Create the Experiment, providing a unique name
+    produce_apples_experiment = client.create_experiment(
+        name="API_Poc_blah", tags=experiment_tags
+    )
+
+
+if __name__ == "__main__":
+    print('Train...')
+    main()
