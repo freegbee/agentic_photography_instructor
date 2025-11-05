31a2c6fd59cd9017cefccf9f160ed17fb145dbd7
Holger Heymanns <holger.heymanns@gmail.com>
2025-11-05 22:14:32 +0100

    chore: lösche alten code

--- src/image_acquisition/acquisition_server/handlers/Div2kHrZipDownloader.py
+++
@@ -1,6 +0,0 @@
-from image_acquisition.acquisition_server.handlers.ZipDownloader import ZipDownloader
-
-
-class Div2kHrZipDownloader(ZipDownloader):
-    def __init__(self):
-        super().__init__('https://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip', '/app/volumes/resources/div2k/hr/validation')
--- src/image_acquisition/acquisition_server/handlers/Places365TarDownloader.py
+++
@@ -1,6 +0,0 @@
-from .TarDownloader import TarDownloader
-
-class Places365TarDownloader(TarDownloader):
-    def __init__(self):
-        super().__init__('https://data.csail.mit.edu/places/places365/val_large.tar', '/app/volumes/resources/places365')
-
