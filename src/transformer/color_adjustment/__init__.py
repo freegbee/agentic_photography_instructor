923d65d87c6501bddef142899e18743d4afe6526
Holger Heymanns <holger.heymanns@gmail.com>
2025-11-16 16:05:08 +0100

    feature: Transformatoren ergänzen
    Notwendig, damit sie via registry verfügbar sind

--- src/transformer/color_adjustment/__init__.py
+++ src/transformer/color_adjustment/__init__.py
@@ -1,7 +1,20 @@
 from .GrayscaleTransformer import GrayscaleTransformer
 from .GrayscaleAverageTransformer import GrayscaleAverageTransformer
+from .SwapColorChannelTransformer import SwapColorChanelTransformerGB, SwapColorChanelTransformerRB, SwapColorChanelTransformerRG
+from .InvertColorChannelTransformer import InvertColorChannelTransformerB, InvertColorChannelTransformerG, InvertColorChannelTransformerR, InvertColorChannelTransformerBG, InvertColorChannelTransformerBR, InvertColorChannelTransformerGR, InvertColorChannelTransformerAll
+
 
 __all__ = [
     "GrayscaleTransformer",
     "GrayscaleAverageTransformer",
+    "SwapColorChanelTransformerGB",
+    "SwapColorChanelTransformerRB",
+    "SwapColorChanelTransformerRG",
+    "InvertColorChannelTransformerB",
+    "InvertColorChannelTransformerG",
+    "InvertColorChannelTransformerR",
+    "InvertColorChannelTransformerBG",
+    "InvertColorChannelTransformerBR",
+    "InvertColorChannelTransformerGR",
+    "InvertColorChannelTransformerAll",
 ]
