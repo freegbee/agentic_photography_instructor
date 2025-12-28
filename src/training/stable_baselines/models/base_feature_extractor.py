c40876a2bff1d44323ac78ff7384dcb43c663fbb
Holger Heymanns <holger.heymanns@gmail.com>
2025-12-28 11:57:46 +0100

    feature: Ergänzung Resnet18 Feature Extractor

--- src/training/stable_baselines/models/base_feature_extractor.py
+++ src/training/stable_baselines/models/base_feature_extractor.py
@@ -1,7 +1,7 @@
 import torch
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from torch import nn
-from torchvision.models import resnet50
+from torchvision.models import resnet50, resnet18
 from torchvision import models
 
 
@@ -35,4 +35,38 @@ class ResNetFeatureExtractor(BaseFeaturesExtractor):
         x = (x - mean) / std
         h = self.backbone(x)  # (B, 2048, 1, 1)
         h = h.reshape(h.size(0), -1)  # (B, 2048)
+        return self.proj(h)  # (B, features_dim)
+
+class ResNet18FeatureExtractor(BaseFeaturesExtractor):
+    """
+        BaseFeaturesExtractor, das ein vortrainiertes ResNet18 als Feature-Backbone nutzt.
+        Erwartet Eingaben im Tensorformat (B, C, H, W) mit Werten in [0,1].
+        """
+
+    def __init__(self, observation_space, features_dim: int = 512, pretrained: bool = True,
+                 freeze_backbone: bool = True):
+        super().__init__(observation_space, features_dim)
+        self.backbone = resnet18(weights = models.ResNet18_Weights.DEFAULT if pretrained else None)
+
+        # Backbone den classfication layer entfernen
+        backbone_feature_dim = int(self.backbone.fc.in_features)  # 512 for ResNet18
+        self.backbone.fc = nn.Identity()
+        if freeze_backbone:
+            for p in self.backbone.parameters():
+                p.requires_grad = False
+        self.proj = nn.Sequential(
+            nn.Linear(backbone_feature_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, features_dim)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # img: numpy array (H, W, C)
+
+        # x: (B, C, H, W), Werte in [0,1]
+        mean = torch.tensor([0.485, 0.456, 0.406], device=x.device).view(1, 3, 1, 1)
+        std = torch.tensor([0.229, 0.224, 0.225], device=x.device).view(1, 3, 1, 1)
+        x = (x - mean) / std
+        h = self.backbone(x)
+        h = h.reshape(h.size(0), -1)
         return self.proj(h)  # (B, features_dim)
