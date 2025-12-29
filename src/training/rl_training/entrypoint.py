eaab4dc2dce5171436639712ea829cda7258dcf0
Holger Heymanns <holger.heymanns@gmail.com>
2025-12-29 12:07:31 +0100

    Feature: Add multi one-step actions support and update training parameters

--- src/training/rl_training/entrypoint.py
+++ src/training/rl_training/entrypoint.py
@@ -3,14 +3,16 @@ from training.rl_training.rl_trainer import RlTrainer
 from training.rl_training.training_params import ImagePreprocessingParams, TransformPreprocessingParams, DataParams, \
     TrainingExecutionParams, GeneralPreprocessingParams
 from training.split_ratios import SplitRatios
-from transformer import POC_TWO_WAY_TRANSFORMERS
+from transformer import POC_MULTI_ONE_STEP_TRANSFORMERS
 from utils.LoggingUtils import configure_logging
 
 configure_logging()
 
+
 def main():
     training_params = HyperparameterRegistry.get_store(TrainingExecutionParams)
-    training_params.set({"experiment_name": "Dynamic_RL_Agent_Training PoC 0.01", "use_local_juror": True, "random_seed": 42})
+    training_params.set(
+        {"experiment_name": "Dynamic_RL_Agent_Training PoC 0.02", "use_local_juror": True, "random_seed": 42})
 
     data_params = HyperparameterRegistry.get_store(DataParams)
     # Mögliche alternative Datensätze:
@@ -26,7 +28,10 @@ def main():
     image_preprocessing_params.set({"batch_size": 64, "resize_max_size": 384})
 
     transform_preprocessing_params = HyperparameterRegistry.get_store(TransformPreprocessingParams)
-    h = {"batch_size": 64, "transformer_names": POC_TWO_WAY_TRANSFORMERS, "use_random_transformer": True, "split": SplitRatios.create_default_split_ratios()}
+    h = {"batch_size": 64,
+         "transformer_names": POC_MULTI_ONE_STEP_TRANSFORMERS,
+         "use_random_transformer": True,
+         "split": SplitRatios.create_default_split_ratios()}
     transform_preprocessing_params.set(h)
 
     trainer = RlTrainer()
