2fa030395b4a70d76b1ab4def30175db7f39fbb9
Holger Heymanns <holger.heymanns@gmail.com>
2026-01-01 23:07:30 +0100

    Add AutoGammaCorrectionTransformer for automatic brightness adjustment

--- src/transformer/lighting/__init__.py
+++ src/transformer/lighting/__init__.py
@@ -14,6 +14,7 @@ from .lightness_transformer import (
     LightnessDecreaseTransformerMedium,
     LightnessDecreaseTransformerStrong
 )
+from .gamma_transformer import AutoGammaCorrectionTransformer
 
 __all__ = [
     "ContrastIncreaseTransformerWeak",
@@ -27,10 +28,12 @@ __all__ = [
     "LightnessIncreaseTransformerStrong",
     "LightnessDecreaseTransformerWeak",
     "LightnessDecreaseTransformerMedium",
-    "LightnessDecreaseTransformerStrong"
+    "LightnessDecreaseTransformerStrong",
+    "AutoGammaCorrectionTransformer"
 ]
 
 SENSIBLE_LIGHTING_TRANSFORMERS = [
     ContrastIncreaseTransformerWeak.label, ContrastDecreaseTransformerWeak.label,
-    LightnessIncreaseTransformerWeak.label, LightnessDecreaseTransformerWeak.label
+    LightnessIncreaseTransformerWeak.label, LightnessDecreaseTransformerWeak.label,
+    AutoGammaCorrectionTransformer.label
 ]
