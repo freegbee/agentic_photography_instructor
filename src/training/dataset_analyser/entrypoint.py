8ef75b4898a084652fd965fa00c8709e6fd7f733
Holger Heymanns <holger.heymanns@gmail.com>
2026-01-16 17:43:11 +0100

    Feature/fiftyone dataset analysis (#30)
    This pull request introduces a new dataset analysis workflow for training data using FiftyOne, including a new AnalysisTrainer class, an entrypoint script for running analyses, and a helper script for launching the FiftyOne app. The main focus is on enabling advanced dataset inspection, uniqueness and clustering analysis, and visualization for machine learning datasets.

---
+++ src/training/dataset_analyser/entrypoint.py
@@ -0,0 +1,30 @@
+import time
+
+from training.dataset_analyser.analysis_trainer import AnalysisTrainer
+from training.hyperparameter_registry import HyperparameterRegistry
+from training.stable_baselines.hyperparameter.data_hyperparams import DataParams
+
+
+def main():
+    # fixiere den data loader als dummy data loader
+    # fixiere einen output path, wo die fiftyone app läuft
+    # instanziiere den TrainingDataSetCreator
+    experiment_name = "Dataset Analyser"
+    run_description = "POC 0.1"
+    run_name = f"{time.strftime('%Y%m%d-%H%M%S')}_{run_description}"
+
+    # Daten & Umgebung
+    dataset_id = "twenty_original_split_amd-win"
+    dataset_id = "flickr2k_big_original_HQ_split_amd-win"
+    dataset_id = "lhq_landscapes_multi_one_step_actions_amd-win"
+    image_size = (384, 384)
+
+    data_params = HyperparameterRegistry.get_store(DataParams)
+    data_params.set({"dataset_id": dataset_id, "image_max_size":image_size})
+
+    trainer = AnalysisTrainer(experiment_name=experiment_name, source_dataset_id=dataset_id, fiftyone_analysis_name='lhq_landscapes_multi_one_step_actions_Dataset', split=True)
+    trainer.run_training(run_name)
+
+
+if __name__ == '__main__':
+    main()
